c944b9cf1832de9b65b7bf7ec7b6acc228f7c2ba
lecik <lec1k2103@gmail.com>
2018-08-09 19:17:51 +0300

    added login

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   before_save { self.email = email.downcase }
   has_secure_password
+
+
+
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
